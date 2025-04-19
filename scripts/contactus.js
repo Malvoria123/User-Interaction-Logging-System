7cad1851ac6d7888a7d14a7d956ab9371d4eadbf
Malvoria123 <102215963+Malvoria123@users.noreply.github.com>
2025-04-19 09:26:06 +0700

    Initial commit: Add website files

---
+++ scripts/contactus.js
@@ -0,0 +1,127 @@
+document.getElementById("nav_home").addEventListener("click", function() {
+    window.location.href = "../sites/home.html";
+});
+document.getElementById("nav_products").addEventListener("click", function() {
+    window.location.href = "../sites/products.html";
+});
+document.getElementById("nav_recipes").addEventListener("click", function() {
+    window.location.href = "../sites/recipes.html";
+});
+document.getElementById("nav_aboutus").addEventListener("click", function() {
+    window.location.href = "../sites/aboutus.html";
+});
+document.getElementById("nav_contactus").addEventListener("click", function() {
+    window.location.href = "../sites/contactus.html";
+});
+
+function validateNumbersOnly(input) {
+    for (var i = 0; i < input.length; i++) {
+        
+        if (isNaN(parseInt(input[i]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function containsSpecialCharacters(input) {
+    var allowedCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    
+    for (var i = 0; i < input.length; i++) {
+        var char = input[i];
+        if (allowedCharacters.indexOf(char) === -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validateData() {
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var cpassword = document.getElementById("cpassword");
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var prob = document.getElementById("prob");
+    var agreement = document.getElementById("agreement");
+    
+    var valfirstname1 = document.getElementById("valfirstname1");
+    var valphone1 = document.getElementById("valphone1");
+    var valphone2 = document.getElementById("valphone2");
+    var valemail1 = document.getElementById("valemail1");
+    var valemail2 = document.getElementById("valemail2");
+    var valpassword1 = document.getElementById("valpassword1");
+    var valpassword2 = document.getElementById("valpassword2");
+    var valcpassword1 = document.getElementById("valcpassword1");
+    var valgender1 = document.getElementById("valgender1");
+    var valprob1 = document.getElementById("valprob1");
+    var valagreement1 = document.getElementById("valagreement1");
+    var temp = 0;
+
+    valfirstname1.style.display = 'none';
+    valphone1.style.display = 'none';
+    valphone2.style.display = 'none';
+    valemail1.style.display = 'none';
+    valemail2.style.display = 'none';
+    valpassword1.style.display = 'none';
+    valpassword2.style.display = 'none';
+    valcpassword1.style.display = 'none';
+    valgender1.style.display = 'none';
+    valprob1.style.display = 'none';
+    valagreement1.style.display = 'none';
+    
+    if (firstname.value === "") {
+        valfirstname1.style.display = 'block';
+        temp++;
+    }
+    if (phone.value === "") {
+        valphone1.style.display = 'block';
+        temp++;
+    }
+
+    if (!validateNumbersOnly(phone.value)) {
+        valphone2.style.display = 'block';
+        temp++;
+    }
+
+    if (email.value === "") {
+        valemail1.style.display = 'block';
+        temp++;
+    } else if (!email.value.endsWith("@email.com")) {
+        valemail2.style.display = 'block';
+        temp++;
+    }
+
+    if (password.value === "") {
+        valpassword1.style.display = 'block';
+        temp++;
+    } else if (!containsSpecialCharacters(password.value)) {
+        valpassword2.style.display = 'block';
+        temp++;
+    } else if (cpassword.value !== password.value) {
+        valcpassword1.style.display = 'block';
+        temp++
+    }
+
+    if (!male.checked && !female.checked) {
+        valgender1.style.display = 'block';
+        temp++;
+    }
+
+    if (prob.value === "") {
+        valprob1.style.display = 'block';
+        temp++;
+    }
+
+    if (!agreement.checked) {
+        valagreement1.style.display = 'block';
+        temp++;
+    }
+
+    if (!temp) {
+        alert("Thank you! Our team will help you as soon as possible!");
+    }
+}
---
+++ scripts/products.js
@@ -0,0 +1,36 @@
+document.getElementById("nav_home").addEventListener("click", function() {
+    window.location.href = "../sites/home.html";
+});
+document.getElementById("nav_products").addEventListener("click", function() {
+    window.location.href = "../sites/products.html";
+});
+document.getElementById("nav_recipes").addEventListener("click", function() {
+    window.location.href = "../sites/recipes.html";
+});
+document.getElementById("nav_aboutus").addEventListener("click", function() {
+    window.location.href = "../sites/aboutus.html";
+});
+document.getElementById("nav_contactus").addEventListener("click", function() {
+    window.location.href = "../sites/contactus.html";
+});
+
+// Get all elements with the 'item_con' class
+const clickableElements = document.querySelectorAll('.item_con');
+
+// Add click event to each element
+clickableElements.forEach(element => {
+    element.addEventListener('click', function() {
+        const divs = this.querySelectorAll('div');
+        
+        const log = {
+            eventType: "click",
+            text: divs.length > 0 ? divs[divs.length - 1].textContent : 'No div Found',
+            timestamp: new Date().toLocaleString(),
+            page: window.location.href,
+            userAgent: navigator.userAgent,
+            screenWidth: screen.width,
+            screenHeight: screen.height
+        };
+        console.log(log);
+    });
+});
