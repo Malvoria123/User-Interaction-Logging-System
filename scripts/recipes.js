21b3edd5637b05dde90e6d0f56d44e8a3f2071cf
Malvoria123 <102215963+Malvoria123@users.noreply.github.com>
2025-04-26 16:23:31 +0700

    adding files

---
+++ scripts/recipes.js
@@ -0,0 +1,15 @@
+document.getElementById("nav_home").addEventListener("click", function() {
+    window.location.href = "../sites/index.html";
+});
+document.getElementById("nav_products").addEventListener("click", function() {
+    window.location.href = "../sites/products.html";
+});
+document.getElementById("nav_recipes").addEventListener("click", function() {
+    window.location.href = "../sites/recipes.html";
+});
+document.getElementById("nav_aboutus").addEventListener("click", function() {
+    window.location.href = "../sites/aboutus.html";
+});
+document.getElementById("nav_contactus").addEventListener("click", function() {
+    window.location.href = "../sites/contactus.html";
+});
