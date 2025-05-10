3dae714c1959de28b6b9eb47678a6df703a91b4d
Malvoria123 <102215963+Malvoria123@users.noreply.github.com>
2025-05-10 16:53:24 +0700

    trying out the link

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -75,7 +75,7 @@ function collect_data() {
 // Function to send interaction data to the server
 async function logInteraction(type, data) {
     try {
-        await fetch("https://interaction-logger-backend.vercel.app/api/", {
+        await fetch("https://interaction-logger-backend.vercel.app/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
